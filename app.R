2d757d585d971b74c6bee194359a70e8084b6174
rabin08 <rabin0208@gmail.com>
2026-05-27 19:41:45 -0700

    Fix deployment

--- app.R
+++ app.R
@@ -1,1 +1,11 @@
+suppressPackageStartupMessages({
+  library(shiny)
+  library(bslib)
+  library(ggplot2)
+  library(dplyr)
+  library(DT)
+  library(bigrquery)
+  library(bsicons)
+})
+
 shiny::shinyAppDir("src")
